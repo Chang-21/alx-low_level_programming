8f53ebf5675cb9c6977ec54805a8eb7039657a4f
Chang <changkuoth80@gmail.com>
2022-06-30 14:25:54 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-gcc
 #include <studio.h>
 int main(void)
 
 {
-	int puts("Programming is like building a multilingual puzzle\n")
-	return (0)
+	int puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }	
