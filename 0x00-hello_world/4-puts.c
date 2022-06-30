235c1c23557c105320b84f79e1dc35588876ada8
Chang <changkuoth80@gmail.com>
2022-06-30 14:59:22 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * return: sucess
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
