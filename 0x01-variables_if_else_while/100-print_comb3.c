ab80906ac1b9ec80143f292818bb52c22bdcf9a7
chang <changkuoth80@gmail.com>
2022-07-01 16:51:29 -0700

    the combs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - the main function of the program is to print
+ *
+ * Return: the program should always end in a 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int coma = 1;
+
+	while (a <= 9)
+	{
+		while (b <= b)
+		{
+			if (coma == 0)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			coma = 0;
+			putchar(a + '0');
+			putchar(b + '0');
+		}
+		++a;
+	}
+	++a;
+	b = 0;
+}
+putchar(10);
+return (0);
+}
