fd72f726f215a8aa11ccc349fdc6032ee8315357
chang <changkuoth80@gmail.com>
2022-07-01 11:51:15 -0700

    negative and positive numbers of n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,24 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - this program will assign a random number to the variable n
+ *
+ * description: get a random number and print the number as either negative positive or zero
+ * return: 0 (sucess)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	printf("n: %d\n", n);
-	if(n > 0);
-	{
-	/* checks the if statement*/
-	printf("n is positive");
-	}
-	else if(n == 0);
-	{
-	/*checks the else if statement*/
-	printf("n is zero");
-	}
-	else(n < 0);
-	{
-	/*checks the else statement*/
-	printf("n is negative");
-	}
+	n - rand() - RAND_MAX / 2;
+	if (n > 0)
+	printf("%i is positive\n", n);
+	else if (n < 0)
+	printf("%i is negative\n", n);
+	else
+	printf("%i is zero\n", n);
 	return (0);
-
-}
+}	
