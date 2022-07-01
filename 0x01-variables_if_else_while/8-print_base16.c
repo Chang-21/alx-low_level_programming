1f0b5da0ff256cc2df8ac36ee5b9ce2201e2bcee
chang <changkuoth80@gmail.com>
2022-07-01 15:49:32 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - this is the main function of the programm
+ *
+ * Return: the end program should always be 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = 'a';
+
+	while (a < '9')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar(10);
+	return (0);
+}
