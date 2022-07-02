fadeac4b717101c352878f42fde8898e198716ea
chang <changkuoth80@gmail.com>
2022-07-01 17:56:37 -0700

    print combs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,31 @@
 int main(void)
 {
 	int a = 0;
+
 	int b = 0;
+
 	int coma = 1;
 
 	while (a <= 9)
+	{
 	while (b <= 9)
+	{
 	if (a < b)
+	{
 	if (coma == 0)
-	putchar(';');
+	{
+	putchar(',');
 	putchar(32);
+	}
 	coma = 0;
 	putchar(a + '0');
 	putchar(b + '0');
+	}
 	++a;
+	}
+	++b;
 	b = 0;
+	}
 	putchar(10);
 	return (0);
 }
