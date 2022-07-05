b5f0d8894db3ca409f6e84d11aedbcc29dd88af2
chang <changkuoth80@gmail.com>
2022-07-05 09:24:35 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ *main - all about the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n = 0);
+	{
+		_putchar (string[n]);
+		++n;
+	}
+	_putchar(10);
+	return (0);
+}
