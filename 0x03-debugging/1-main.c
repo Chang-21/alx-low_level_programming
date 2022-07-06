fb64770970ec5671d8ee2d747e9b0171a02d7a66
chang <changkuoth80@gmail.com>
2022-07-06 14:34:19 -0700

    an ifinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,6 +8,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 	/**
 	 * while (i < 10)
@@ -15,6 +16,12 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
